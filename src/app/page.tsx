fea50892d5e90403d621bde636dbdcd5203b0c58
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-07-11 15:57:36 -0300

    Adiciona botão "Invoke Background Job" à página inicial, melhorando a interatividade da interface.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-  return <div> Home </div>;
+  return (
+    <div className="p-4 max-w-7xl mx-auto">
+      {' '}
+      <Button>Invoke Background Job</Button>{' '}
+    </div>
+  );
 }
