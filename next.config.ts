0c7eafab439cefecb3d8e4d15b5d1076f1615eb6
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-07-11 15:22:15 -0300

    Adiciona configuração do ESLint para incluir diretórios específicos, melhorando a análise de código e a qualidade do projeto.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    dirs: ['src/app', 'src/components', 'src/hooks', 'src/lib', 'src/trpc'],
+  },
 };
 
 export default nextConfig;
