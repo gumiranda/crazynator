42c52ecc497ecb2df7da90c1433148512286a947
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-07-11 15:22:41 -0300

    Atualiza a configuração do ESLint para incluir apenas o diretório 'src', simplificando a análise de código e focando na estrutura principal do projeto.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   eslint: {
-    dirs: ['src/app', 'src/components', 'src/hooks', 'src/lib', 'src/trpc'],
+    dirs: ['src'],
   },
 };
 
