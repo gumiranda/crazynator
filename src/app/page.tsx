2202541347297d2ef7d20584b383224026c377c1
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-07-11 15:59:24 -0300

    Adiciona funcionalidade de mutação ao botão "Invoke Background Job", permitindo o envio de dados ao backend ao ser clicado.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
+import { useMutation } from '@tanstack/react-query';
+import { useTRPC } from '@/trpc/client';
 
 export default function Home() {
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({}));
   return (
     <div className="p-4 max-w-7xl mx-auto">
-      {' '}
-      <Button>Invoke Background Job</Button>{' '}
+      <Button onClick={() => invoke.mutate({ text: 'devdoido' })}>Invoke Background Job</Button>
     </div>
   );
 }
