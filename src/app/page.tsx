bbb75ba90e1227cb35a275251ac61d60a9efbf7d
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-07-11 15:56:36 -0300

    Remove o componente ClientComponent e simplifica a página inicial, substituindo seu conteúdo por um texto estático.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-import { getQueryClient, trpc } from '@/trpc/server';
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
-import ClientComponent from './client';
-import { Suspense } from 'react';
+'use client';
 
-export default async function Home() {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.devDoido.queryOptions({ text: 'falaaa dev doido PREFETCH' }));
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <ClientComponent />
-      </Suspense>
-    </HydrationBoundary>
-  );
+export default function Home() {
+  return <div> Home </div>;
 }
