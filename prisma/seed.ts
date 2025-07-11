629da29fa783b1f4cd81fa1ee9afba10b498c4d1
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-07-11 07:28:11 -0300

    Adiciona modelos User e Post ao schema do Prisma, implementa script de seed para criar um usuário padrão e inclui arquivos de migração para a criação das tabelas correspondentes no banco de dados.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,6 +2,10 @@ import { PrismaClient } from "@/generated/prisma";
 const prisma = new PrismaClient();
 
 async function main() {
-    
+    await prisma.user.create({
+        data: {
+            name: "John Doe",
+        },
+    });
 }   
 main()
