e69e1b3f5da93b284022365405986ec96f585c34
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-07-11 10:57:25 -0300

    Atualiza a configuração do VSCode para incluir o TypeScript SDK e habilita o uso do TypeScript do workspace. Modifica a rota do TRPC, renomeando de 'hello' para 'devDoido', e adiciona uma consulta no componente Home para testar a nova rota.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import { Button } from '@/components/ui/button';
+import { useTRPC } from '@/trpc/client';
 
 export default function Home() {
+  const trpc = useTRPC();
+  trpc.devDoido.queryOptions({ text: 'falaaa dev doido' });
   return (
     <div>
       <Button>Hello World</Button>
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 import { baseProcedure, createTRPCRouter } from '../init';
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
+  devDoido: baseProcedure
     .input(
       z.object({
         text: z.string(),
