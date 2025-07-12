447d522afed45c5195387c748de36b0270323188
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-07-12 17:35:40 -0300

    Remove a inclusão da entidade Fragment no procedimento de recuperação de mensagens, simplificando a estrutura do código e mantendo a funcionalidade de ordenação por data de atualização.

--- src/modules/messages/server/procedures.ts
+++ src/modules/messages/server/procedures.ts
@@ -9,9 +9,6 @@ export const messagesRouter = createTRPCRouter({
       orderBy: {
         updatedAt: 'asc',
       },
-      include: {
-        fragment: true,
-      },
     });
     return messages;
   }),
