2a896e728acb9b44ddf8a8b934b6d033aa3ced73
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-07-15 08:10:22 -0300

    Atualiza o arquivo comandos.txt para incluir a biblioteca date-fns na instalação de random-word-slugs. Adiciona componentes UserMessage e AssistantMessage ao MessageCard para exibir mensagens de forma diferenciada, melhorando a apresentação e a clareza das interações entre usuário e assistente.

--- src/modules/projects/ui/components/message-card.tsx
+++ src/modules/projects/ui/components/message-card.tsx
@@ -1,4 +1,55 @@
+import { Card, CardContent } from '@/components/ui/card';
 import { Fragment, MessageRole, MessageType } from '@/generated/prisma';
+import { cn } from '@/lib/utils';
+import { format } from 'date-fns';
+interface UserMessageProps {
+  content: string;
+}
+const UserMessage = ({ content }: UserMessageProps) => {
+  return (
+    <div className="flex justify-end pb-4 pr-2 pl-10">
+      <Card className="rounded-lg bg-muted p-3 shadow-none border-none max-w-[80%] break-words">
+        <CardContent>{content}</CardContent>
+      </Card>
+    </div>
+  );
+};
+
+interface AssistantMessageProps {
+  content: string;
+  fragment: Fragment;
+  createdAt: Date;
+  isActiveFragment: boolean;
+  onFragmentClick: (fragment: Fragment) => void;
+  type: MessageType;
+}
+const AssistantMessage = ({
+  content,
+  fragment,
+  createdAt,
+  isActiveFragment,
+  onFragmentClick,
+  type,
+}: AssistantMessageProps) => {
+  return (
+    <div
+      className={cn(
+        'flex flex-col group px-4 pb-4',
+        type === 'ERROR' && 'text-red-700 dark:text-red-500',
+      )}
+    >
+      <div className="flex items-center gap-2 pl-2 mb-2">
+        <span className="text-sm font-medium">Crazy Code</span>
+        <span className="text-xs text-muted-foreground opacity-0 transition-opacity group-hover:opacity-100">
+          {format(createdAt, "HH:mm 'on' MMM dd, yyyy")}
+        </span>
+      </div>
+      <div className="pl-8.5 flex flex-col gap-y-4">
+        <span>{content}</span>
+      </div>
+    </div>
+  );
+};
 
 interface MessageCardProps {
   content: string;
@@ -20,7 +71,16 @@ export const MessageCard = ({
   type,
 }: MessageCardProps) => {
   if (role === 'ASSISTANT') {
-    return <p>ASSISTANT</p>;
+    return (
+      <AssistantMessage
+        content={content}
+        fragment={fragment}
+        createdAt={createdAt}
+        isActiveFragment={isActiveFragment}
+        onFragmentClick={onFragmentClick}
+        type={type}
+      />
+    );
   }
-  return <div>USER</div>;
+  return <UserMessage content={content} />;
 };
