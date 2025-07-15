e7bc9fb3ed6c2e24da5dffd75b80652c23aa83c9
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-07-15 07:07:00 -0300

    Remove a consulta TRPC e o uso do hook useSuspenseQuery do componente ProjectView, simplificando a lógica de recuperação de dados. Essa alteração melhora a clareza do código e prepara o componente para uma abordagem de carregamento mais eficiente.

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -1,6 +1,4 @@
 'use client';
-import { useTRPC } from '@/trpc/client';
-import { useSuspenseQuery } from '@tanstack/react-query';
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable';
 import { MessagesContainer } from '../components/messages-container';
 import { Suspense } from 'react';
@@ -9,12 +7,6 @@ interface Props {
 }
 
 export const ProjectView = ({ projectId }: Props) => {
-  const trpc = useTRPC();
-  const { data: project } = useSuspenseQuery(
-    trpc.projects.getOne.queryOptions({
-      id: projectId,
-    }),
-  );
   return (
     <div className="h-screen">
       <ResizablePanelGroup direction="horizontal">
