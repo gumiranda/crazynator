aa9c084ffa46b6fdfee12c304a18f3760293ec02
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-07-15 06:55:23 -0300

    Atualiza a página do projeto para utilizar o componente ProjectView dentro de um HydrationBoundary, melhorando a exibição dos detalhes do projeto e a gestão do estado da consulta. Essa mudança aprimora a estrutura da aplicação e a experiência do usuário ao carregar os dados.

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,5 +1,7 @@
+import { ProjectView } from '@/modules/projects/ui/views/project-view';
 import { trpc } from '@/trpc/server';
 import { getQueryClient } from '@/trpc/server';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 
 interface ProjectPageProps {
   params: {
@@ -20,5 +22,9 @@ export default async function ProjectPage({ params }: ProjectPageProps) {
       id: projectId,
     }),
   );
-  return <div>ProjectPage {projectId}</div>;
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ProjectView projectId={projectId} />
+    </HydrationBoundary>
+  );
 }
