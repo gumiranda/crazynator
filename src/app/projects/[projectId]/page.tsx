3a753efe2f14481e29aa5bbf7bf4a4fc2f8060d0
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-07-15 06:56:07 -0300

    Adiciona o componente Suspense ao redor do ProjectView na página do projeto, melhorando a experiência do usuário ao exibir um fallback de carregamento enquanto os dados estão sendo recuperados. Essa alteração aprimora a gestão do estado durante a renderização dos detalhes do projeto.

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -2,6 +2,7 @@ import { ProjectView } from '@/modules/projects/ui/views/project-view';
 import { trpc } from '@/trpc/server';
 import { getQueryClient } from '@/trpc/server';
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { Suspense } from 'react';
 
 interface ProjectPageProps {
   params: {
@@ -24,7 +25,9 @@ export default async function ProjectPage({ params }: ProjectPageProps) {
   );
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <ProjectView projectId={projectId} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ProjectView projectId={projectId} />
+      </Suspense>
     </HydrationBoundary>
   );
 }
