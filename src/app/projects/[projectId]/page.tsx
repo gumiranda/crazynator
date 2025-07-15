b7ab334d14c694634702a254e3396cfea05365b2
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-07-15 06:49:40 -0300

    Adiciona pré-busca de dados na página do projeto, incluindo consultas para mensagens e detalhes do projeto. Implementa tratamento de erro no procedimento de recuperação de um único projeto, melhorando a robustez e a experiência do usuário.

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,3 +1,6 @@
+import { trpc } from '@/trpc/server';
+import { getQueryClient } from '@/trpc/server';
+
 interface ProjectPageProps {
   params: {
     projectId: string;
@@ -6,5 +9,16 @@ interface ProjectPageProps {
 
 export default async function ProjectPage({ params }: ProjectPageProps) {
   const { projectId } = await params;
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.messages.getMany.queryOptions({
+      projectId,
+    }),
+  );
+  void queryClient.prefetchQuery(
+    trpc.projects.getOne.queryOptions({
+      id: projectId,
+    }),
+  );
   return <div>ProjectPage {projectId}</div>;
 }
--- src/modules/projects/server/procedures.ts
+++ src/modules/projects/server/procedures.ts
@@ -3,8 +3,29 @@ import { z } from 'zod';
 import { prisma } from '@/lib/db';
 import { inngest } from '@/inngest/client';
 import { generateSlug } from 'random-word-slugs';
+import { TRPCError } from '@trpc/server';
 
 export const projectsRouter = createTRPCRouter({
+  getOne: baseProcedure
+    .input(
+      z.object({
+        id: z.string().min(1, { message: 'Project is required' }),
+      }),
+    )
+    .query(async ({ input }) => {
+      const project = await prisma.project.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+      if (!project) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: 'Project not found',
+        });
+      }
+      return project;
+    }),
   getMany: baseProcedure.query(async () => {
     const projects = await prisma.project.findMany({
       orderBy: {
