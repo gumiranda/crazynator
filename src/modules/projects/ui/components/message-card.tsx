4ca3bcedb8f6ff91916b557d16d2f4fcc106987e
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-07-16 07:56:43 -0300

    Adiciona novos arquivos de logo (logo-auth.png e logo.svg) e implementa o componente FragmentCard no MessageCard, permitindo a exibição de fragmentos de mensagens de forma interativa. Essa atualização melhora a apresentação visual e a usabilidade do componente.

--- src/modules/projects/ui/components/message-card.tsx
+++ src/modules/projects/ui/components/message-card.tsx
@@ -2,6 +2,8 @@ import { Card, CardContent } from '@/components/ui/card';
 import { Fragment, MessageRole, MessageType } from '@/generated/prisma';
 import { cn } from '@/lib/utils';
 import { format } from 'date-fns';
+import { ChevronRightIcon, Code2Icon } from 'lucide-react';
+import Image from 'next/image';
 interface UserMessageProps {
   content: string;
 }
@@ -12,6 +14,32 @@ const UserMessage = ({ content }: UserMessageProps) => {
         <CardContent>{content}</CardContent>
       </Card>
     </div>
+  );
+};
+
+interface FragmentCardProps {
+  fragment: Fragment;
+  isActiveFragment: boolean;
+  onFragmentClick: (fragment: Fragment) => void;
+}
+const FragmentCard = ({ fragment, isActiveFragment, onFragmentClick }: FragmentCardProps) => {
+  return (
+    <button
+      className={cn(
+        'flex items-start text-start gap-2 border rounded-lg bg-muted w-fit p-3 hover:bg-secondary transition-colors cursor-pointer',
+        isActiveFragment && 'bg-primary text-primary-foreground border-primary hover:bg-primary',
+      )}
+      onClick={() => onFragmentClick(fragment)}
+    >
+      <Code2Icon className="w-4 h-4 mt-0.5" />
+      <div className="flex flex-col flex-1">
+        <span className="text-sm font-medium line-clamp-1">{fragment.title}</span>
+        <span className="text-sm">Preview</span>
+      </div>
+      <div className="flex items-center justify-center mt-0.5">
+        <ChevronRightIcon className="w-4 h-4" />
+      </div>
+    </button>
   );
 };
 
@@ -39,6 +67,7 @@ const AssistantMessage = ({
       )}
     >
       <div className="flex items-center gap-2 pl-2 mb-2">
+        <Image src="/logo.svg" alt="Crazy Code" width={18} height={18} className="shrink-0" />
         <span className="text-sm font-medium">Crazy Code</span>
         <span className="text-xs text-muted-foreground opacity-0 transition-opacity group-hover:opacity-100">
           {format(createdAt, "HH:mm 'on' MMM dd, yyyy")}
@@ -46,6 +75,13 @@ const AssistantMessage = ({
       </div>
       <div className="pl-8.5 flex flex-col gap-y-4">
         <span>{content}</span>
+        {fragment && type === 'RESULT' && (
+          <FragmentCard
+            fragment={fragment}
+            isActiveFragment={isActiveFragment}
+            onFragmentClick={onFragmentClick}
+          />
+        )}
       </div>
     </div>
   );
