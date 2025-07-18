f9f07e23cf8b9f8708dcf3d90ba70582658335fe
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-07-18 16:00:17 -0300

    Atualiza o componente ProjectsList para exibir o nome do usuário autenticado e implementa verificação de autenticação, retornando null caso o usuário não esteja logado. Essa mudança melhora a personalização da interface e a experiência do usuário ao interagir com seus projetos.

--- src/modules/home/ui/components/projects-list.tsx
+++ src/modules/home/ui/components/projects-list.tsx
@@ -6,15 +6,16 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useTRPC } from '@/trpc/client';
 import { Button } from '@/components/ui/button';
+import { useUser } from '@clerk/nextjs';
 
 export default function ProjectList() {
   const trpc = useTRPC();
-
+  const { user } = useUser();
   const { data: projects } = useQuery(trpc.projects.getMany.queryOptions());
-
+  if (!user) return null;
   return (
     <div className="dark:bg-sidebar flex w-full flex-col gap-y-6 rounded-xl border bg-white p-8 sm:gap-y-4">
-      <h2 className="text-2xl font-semibold">Gustav&apos;s Saved Projects</h2>
+      <h2 className="text-2xl font-semibold">{user?.firstName}&apos;s Crazy Projects</h2>
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-3">
         {!projects || projects.length === 0 ? (
           <div className="col-span-full text-center">
