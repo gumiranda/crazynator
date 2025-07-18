0660117bba69341bd90bf0d96199972280789ede
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-07-18 17:07:39 -0300

    Adiciona o router `usagesRouter` ao `appRouter`, integrando a funcionalidade de rastreamento de uso ao sistema. Essa mudança melhora a gestão de recursos, permitindo acesso às informações de uso de forma mais eficiente.

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,12 @@
 import { createTRPCRouter } from '../init';
 import { messagesRouter } from '@/modules/messages/server/procedures';
 import { projectsRouter } from '@/modules/projects/server/procedures';
+import { usagesRouter } from '@/modules/usage/server/procedures';
+
 export const appRouter = createTRPCRouter({
   messages: messagesRouter,
   projects: projectsRouter,
+  usages: usagesRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
